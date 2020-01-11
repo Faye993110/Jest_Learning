1015299b5b00e90333b0cb35cfe098d4f1adc4d6
Faye993110 <470917066@qq.com>
2020-01-11 23:17:00 +0800

    mock timers

---
+++ lesson3/timer.test.js
@@ -0,0 +1,22 @@
+import timer from './timer'
+jest.useFakeTimers();
+
+// test('timer 测试', (done) => {
+//     timer( () => {
+//         expect(1).toBe(1);
+//         done();
+//     })
+// })
+
+test('timer 测试', () => {
+    const fn = jest.fn();
+    timer(fn);
+    //jest.runAllTimers();
+    //jest.runOnlyPendingTimers();//当前处于队列中的timer
+    jest.advanceTimersByTime(3000);
+    expect(fn).toHaveBeenCalledTimes(1);
+    jest.advanceTimersByTime(3000);
+    expect(fn).toHaveBeenCalledTimes(2);
+})
+
+//useFakeTimers 和 runAllTimers 配对执行
